a49ae1e7495d6d46d0e1c029005f558d6597dab0
dalikewara <dalikewara@gmail.com>
2020-12-01 05:50:39 +0700

    remove scripts folder

--- scripts/complete-release-single-execution.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git checkout development && git add . && git add -u && 
-git commit -m "update plugin: documentation" && git push origin development && git checkout testing && git merge development && git push origin testing && git checkout master && git merge testing && git push origin master &&
-git tag -a v3.0.20 -m "ayapingping-js v3.0.20" && git push --tags && npm publish && git checkout development && git branch
