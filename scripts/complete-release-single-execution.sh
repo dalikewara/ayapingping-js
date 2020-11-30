9061586365ba7ea2e811f4b2831bd8a1a06c4cb2
dalikewara <dalikewara@gmail.com>
2020-12-01 05:21:35 +0700

    update plugin: documentation

--- scripts/complete-release-single-execution.sh
+++ scripts/complete-release-single-execution.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git checkout development && git add . && git add -u && 
-git commit -m "update plugin: allowedContentType" && git push origin development && git checkout testing && git merge development && git push origin testing && git checkout master && git merge testing && git push origin master &&
-git tag -a v3.0.18 -m "ayapingping-js v3.0.18" && git push --tags && npm publish && git checkout development && git branch
+git commit -m "update plugin: documentation" && git push origin development && git checkout testing && git merge development && git push origin testing && git checkout master && git merge testing && git push origin master &&
+git tag -a v3.0.19 -m "ayapingping-js v3.0.19" && git push --tags && npm publish && git checkout development && git branch
